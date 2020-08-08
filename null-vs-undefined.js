a548d2364d77eced72dd0c206d318bf2c68628f6
Md. Mostafizur Rahman <dev.mostafizur@gmail.com>
2020-08-08 11:23:50 +0600

    truthy-falsy | null-vs-undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,13 @@
+function add(num1, num2){
+    //console.log(num1 + num2);
+    console.log(num1, num2);
+    //return num1 + num2;
+}
+//const result = add(45);
+//console.log(result);
+
+const car = {color:"Blue", price: 200000};
+//console.log(car.color);
+console.log(car.category);
+
+// null can explicitly set as value
---
+++ truthy-falsy.js
@@ -0,0 +1,25 @@
+// Falsy Values
+    // false
+    // 0
+    // ""
+    // undefined
+    // null
+    // NaN
+
+// Truthy
+    // '0'
+    // ' '
+    // []
+
+//let a = 0;
+//let a = false;
+//let a = '';
+//let a = undefined;
+//let a = null;
+//let a = NaN;
+let a = [];
+if(a){
+    console.log("This is true");
+} else{
+    console.log("This is false");
+}
